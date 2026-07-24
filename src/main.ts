f42532a42f7869bd523790cc688a5f23ede4c111
Shubert Munthali <shubertm.m@gmail.com>
2026-07-24 20:54:17 +0200

    feat: Heading (#3)
    * Add wabsite heading
    
    * Add subheading

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
-import kadePayRail from './assets/kadepay_rail.svg'
 import kadePay from './assets/kadepay.svg'
+
+let h3 = "No nonsense";
+let count = 0;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <header>
@@ -8,19 +10,41 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 </header>
 
 <section id="center">
-  <div class="hero">
-    <img src="${kadePayRail}" alt="KadePay Rail"/>
-  </div>
   <div>
-    <h1>Fast Bitcoin, the Arkade way</h1>
+    <h1>No Bitcoin. No Better Money</h1>
+    <h2>Payments redefined, the Bitcoin way.</h2>
+    <h3 id="subheading2">${h3}</h3>
   </div>
 </section>
-
-<section id="next-steps">
+<!--section id="next-steps">
   <div id="social">
     <ul>
       <li><a href="https://github.com/kadepayments" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
     </ul>
   </div>
-</section>
+</section-->
 `
+
+const subHeadings = [
+    "Your money",
+    "Smart",
+    "Your control",
+    "No middlemen",
+    "No nonsense",
+];
+
+const h3Element = document.querySelector<HTMLDivElement>('#subheading2');
+const timerId = setInterval(() => {
+    count++
+    if (count > 4) {
+        count = 0;
+    }
+    const subHeading = subHeadings[count];
+    if (h3Element) {
+        h3Element.textContent = `${subHeading}`;
+    }
+}, 3000);
+
+addEventListener('beforeunload', () => {
+    clearInterval(timerId);
+})
